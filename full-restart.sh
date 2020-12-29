961716bf20261751c15e412133ce546c5f912268
VForsmann <vitus.forsmann@hotmail.de>
2020-12-29 15:05:49 +0100

    seperate feathers from nginx/certbot setup

--- full-restart.sh
+++ full-restart.sh
@@ -11,6 +11,10 @@ data_path="./data/certbot"
 email="vf691184@fh-muenster.de" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
+echo "Starting Nodejs and Mongodb... Setting up app-network..."
+docker-compose -f docker-compose-prod-feathers.yml up
+echo
+
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
