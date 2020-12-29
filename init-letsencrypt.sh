50ff2f0ffacaf7a0cb64a160586c23568ceca416
VForsmann <vitus.forsmann@hotmail.de>
2020-12-29 14:13:53 +0100

    small adjustments

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(www.lawy.tk lawy.tk)
+domains=(lawy.tk www.lawy.tk)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="vf691184@fh-muenster.de" # Adding a valid address is strongly recommended
