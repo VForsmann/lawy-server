bcfd5887191ad68abede14cc9e44991b3c36501a
VForsmann <vitus.forsmann@hotmail.de>
2020-12-29 18:07:13 +0100

    new domain

--- full-restart.sh
+++ full-restart.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(lawy.tk www.lawy.tk)
+domains=(lawy.ml www.lawy.ml)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="vf691184@fh-muenster.de" # Adding a valid address is strongly recommended
